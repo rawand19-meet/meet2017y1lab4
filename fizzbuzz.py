b446d4b4101d90a4cf10259e4f2407b3ade9a2cd
rawand19-meet <rawand19@meet.mit.edu>
2017-07-26 12:24:47 +0300

    Stopping here

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+for i in range (100):
+    count=i+1
+    print(cout)
+
+for i in range (100):
+    count=i+1
+    if count%3==0:
+        print('fizz')
+    else:
+         print(count)
+for i in range (101):
+     count =i+1
+     if count % 3==0:
+          print('fizz')
+     elif  3:
+         print('buzz')
+
+      else 5 :
+          print(count)
+           
---
+++ fruit_sorter.py
@@ -0,0 +1,9 @@
+new_fruit=input('what fruit am I storing?')
+if apples:
+    print('Bin1')
+elif oranges:
+    print('Bin2')
+else:
+    print('error!Ido ot recognise this fruit!')
+    
+    
